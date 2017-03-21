a1f4c0de810ea4d252bb3c1fce1e18fdc76fb97e
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2017-03-22 01:06:47 +0800

    Remove cookie support

--- src/util/persistors.js
+++ src/util/persistors.js
@@ -1,35 +1,11 @@
-export const isLocalStorageSupported = (typeof(Storage) !== 'undefined' && window['localStorage'] != null )
-
 const genLocalStorageKey = key => `react-transmission.${key}`
 
 export function persistKey (key, val) {
-  if (isLocalStorageSupported) {
-    window.localStorage.setItem(genLocalStorageKey(key), val);
-  } else {
-    const date = new Date();
-    date.setFullYear(date.getFullYear() + 1);
-    document.cookie = `${key}=${val}; expires=${date.toGMTString()}; path=/`;
-  }
+  window.localStorage.setItem(genLocalStorageKey(key), val);
 }
 
 export function rehydrateKey (key, fallback) {
-  if (isLocalStorageSupported) {
-    return window.localStorage.getItem(genLocalStorageKey(key)) || getFromCookieKey(key, fallback);
-  } else {
-    getFromCookieKey(key, fallback);
-  }
-}
-
-function getFromCookieKey (key, fallback) {
-  let val;
-  let lines = document.cookie.split(';');
-  for (let i = 0; !val && i < lines.length; ++i) {
-    let line = lines[i].trim();
-    let delim = line.indexOf('=');
-    if ((delim === key.length) && line.indexOf(key) === 0) {
-        val = line.substring(delim + 1);
-    };
-  };
+  let val = window.localStorage.getItem(genLocalStorageKey(key));
 
   // FIXME: we support strings and booleans... add number support too?
   if (!val) {
